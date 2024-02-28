e40449ba88953a1dcf27689c6bc8256c2161d777
Adrian Williams <adrianwilliams1389@gmail.com>
2024-02-27 19:55:51 -0600

    Added Javascript

---
+++ scripts/site.js
@@ -0,0 +1,17 @@
+function ClearForm(){
+    //document.getElementById("fullname").value = '';
+    //document.getElementById("email").value = '';
+    //document.getElementById("inquiry").value = '';
+    document.getElementById("contactForm").reset();
+}
+function hidePTags(){
+    let ptags = document.getElementsByTagName('p');
+    for(let item of ptags){
+        //item.style.visibility = 'hidden'; // removes element, leaves space
+        item.style.display = 'none'; // removes element and space
+    }
+}
+
+$('#jqueryBtn').click(function (){
+    $('p').hide();
+})
